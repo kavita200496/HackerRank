77f8b612f7759d30f047fe1922cfbea9658e5cb2
Kavita Kumari <54909292+kavita200496@users.noreply.github.com>
2020-07-15 11:35:47 +0530

    Create 2D Array - DS.cpp

---
+++ 2D Array - DS.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the hourglassSum function below.
+int hourglassSum(vector<vector<int>> arr) {
+    int res = INT_MIN;
+
+    for (int i = 0; i < 4; i++) {
+        int tmp = 0;
+        for (int j = 0; j < 4; j++) {
+            tmp = arr[i][j] + arr[i][j+1]+arr[i][j+2];
+            tmp += arr[i+1][j+1];
+            tmp += arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+            res = max(res, tmp);
+        }
+        
+    }
+return res;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = hourglassSum(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
